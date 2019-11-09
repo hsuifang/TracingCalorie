d518d41eab23a60280d01675bffe6cef4be25362
hsuifang <ifang_hsu@hpicorp.com.tw>
2019-11-09 19:59:09 +0800

    [Add]: DEL AND CLEAR ALL FROM LOCAL

--- app.js
+++ app.js
@@ -31,6 +31,27 @@ const StorageCtrl = (function() {
       }
 
       return items
+    },
+    updateItemStorage: function (updatedItem) {
+      let items = JSON.parse(localStorage.getItem('items'))
+      items.forEach(function(item, index) {
+        if(updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem)
+        }
+      })
+      localStorage.setItem('items', JSON.stringify(items))
+    },
+    deleteItemFromStorage: function (id) {
+      let items = JSON.parse(localStorage.getItem('items'))
+      items.forEach(function(item, index) {
+        if(id === item.id) {
+          items.splice(index, 1)
+        }
+      })
+      localStorage.setItem('items', JSON.stringify(items))
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem('items')
     }
   }
 })()
@@ -378,6 +399,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories)
 
+    // Update local storage
+    StorageCtrl.updateItemStorage(updateItem)
+
     UICtrl.clearEditState()
 
     e.preventDefault()
@@ -399,6 +423,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
 
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories)
+
+    // Delete from local storage
+    StorageCtrl.deleteItemFromStorage(currentItem.id)
 
     UICtrl.clearEditState()
 
@@ -423,6 +450,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
 
     // Hide UL
     UICtrl.hideList()
+
+    // Clear from localstorage
+    StorageCtrl.clearItemsFromStorage()
 
     e.preventDefault()
   }
